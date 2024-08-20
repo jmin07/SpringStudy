ae03b6b3b0ee2ee84df7eb75b3f52182b5f498d5
jmin07 <jmbaek07@gmail.com>
2024-08-20 23:33:35 +0900

    first commit

---
+++ SpringSecurityOAuth/src/main/java/com/test/oauth/LearnSpringOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.test.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringOAuthApplication.class, args);
+	}
+
+}
